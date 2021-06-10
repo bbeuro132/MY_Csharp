a959ee9a48950a9df28792e7bffc74f85c17a982
bbeuro132 <81130257+bbeuro132@users.noreply.github.com>
2021-06-10 16:10:23 +0900

    싱글톤 연습

---
+++ singletoneTEST/singletoneTEST/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singletoneTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var object1 = SingleTEST.getInstance().getData();
+            var object2 = SingleTEST.getInstance().getData();
+
+            Console.WriteLine("object1 : " + object1);
+            Console.WriteLine("object2 : " + object2);
+
+        }
+    }
+}
---
+++ singletoneTEST/singletoneTEST/SingleTEST.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singletoneTEST
+{
+    class SingleTEST
+    {
+        Random r = new Random();
+        private static SingleTEST inst;
+        private int data;
+        SingleTEST()
+        {
+            data = r.Next(100) * 100;
+        }
+
+        public static SingleTEST getInstance()
+        {
+            if (inst == null)
+            {
+                inst = new SingleTEST();
+            }
+            return inst;
+        }
+        public int getData()
+        {
+            return data;
+        }
+    }
+}
