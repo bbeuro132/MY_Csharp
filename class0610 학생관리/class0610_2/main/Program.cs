c4cea93deb27a2648e25e22ae0fea3b42cc6ca45
bbeuro132 <81130257+bbeuro132@users.noreply.github.com>
2021-06-10 16:08:51 +0900

    학생관리 실습

---
+++ class0610 학생관리/class0610_2/main/Program.cs
@@ -0,0 +1,27 @@
+﻿using class0610_2.model;
+using class0610_2.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class0610_2.main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] st = new Student[10];
+            RandData r = new RandData();
+
+            for (int i = 0; i < st.Length; i++)
+            {
+                st[i] = new Student(r.getName(), r.getAge(), r.getGender(), r.getAddress());
+                Console.WriteLine((i + 1) + "번째 학생 출력");
+                st[i].showStudentInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ class0610 학생관리/class0610_2/util/RandData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class0610_2.util
+{
+    class RandData
+    {
+        Random rand = new Random();
+
+        static string[] name = { "홍길동", "김길동", "박길동", "최길동", "이길동" };
+        static int[] age = { 20, 25, 30, 35, 40 };
+        static char[] gender = { '남', '여' };
+        static string[] address = { "대구시 동구 신천3동", "대구시 동구 신천4동",
+                            "대구시 동구 신천5동", "대구시 동구 신천6동", 
+                            "대구시 동구 신천7동"};
+
+        public string getName()
+        {
+            return name[rand.Next(name.Length)];
+        }
+        public int getAge()
+        {
+            return age[rand.Next(age.Length)];
+        }
+        public char getGender()
+        {
+            return gender[rand.Next(gender.Length)];
+        }
+        public string getAddress()
+        {
+            return address[rand.Next(address.Length)];
+        }
+    }
+}
